ef49b0323e1efa1b275deef913efc226518f10c3
Piyaporn <piyapornlahr@gmail.com>
2023-06-01 17:34:46 +0100

    Created backend

---
+++ recipes-backend/src/main/java/nology/project/RecepiesApplication.java
@@ -0,0 +1,13 @@
+package nology.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecepiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecepiesApplication.class, args);
+	}
+
+}
---
+++ recipes-backend/src/test/java/nology/project/RecepiesApplicationTests.java
@@ -0,0 +1,13 @@
+package nology.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecepiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
