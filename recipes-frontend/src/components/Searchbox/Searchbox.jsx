6ab793784c859d452c834800f2e819fc1539f77a
Piyaporn <piyapornlahr@gmail.com>
2023-06-02 15:10:28 +0100

    Searchbox created with handleSearchInput function

---
+++ recipes-frontend/src/components/Searchbox/Searchbox.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Searchbox.scss";
+import Search from "../../assets/images/search.png";
+
+const Searchbox = ({ handleSearchInput, searchTerm }) => {
+  return (
+    <div className="search">
+      <div className="searchbox">
+        <div className="searchbox__wrap">
+          <input
+            className="searchbox__wrap--input"
+            type="text"
+            placeholder="Search for the recipe.."
+            onInput={handleSearchInput}
+            value={searchTerm}
+          />
+          <img
+            className="searchbox__wrap--icon"
+            src={Search}
+            alt="search icon"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Searchbox;
--- recipes-frontend/src/pages/Foods/Foods.jsx
+++ recipes-frontend/src/pages/Foods/Foods.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import { React, useState } from "react";
 import "./Foods.scss";
+import Heading from "../../components/Heading/Heading";
+import Searchbox from "../../components/Searchbox/Searchbox";
 
 const Foods = () => {
-  return <div>
-    <h1>Recipes</h1>
-  </div>;
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearchInput = (event) => {
+    const lowerCaseSearchedResult = event.target.value.toLowerCase();
+    setSearchTerm(lowerCaseSearchedResult);
+  };
+  return (
+    <div>
+      <div>
+        <Heading />
+      </div>
+      <div>
+        <Searchbox searchTerms={searchTerm}
+        handleSearchInput = {handleSearchInput}/>
+      </div>
+    </div>
+  );
 };
 
 export default Foods;
