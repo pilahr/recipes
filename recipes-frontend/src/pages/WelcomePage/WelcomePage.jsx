59614fc30350403cea1bed565a335b9b0d4a0411
Piyaporn <piyapornlahr@gmail.com>
2023-06-02 15:53:48 +0100

    Added link to the round button on welcome page

--- recipes-frontend/src/pages/WelcomePage/WelcomePage.jsx
+++ recipes-frontend/src/pages/WelcomePage/WelcomePage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./WelcomePage.scss";
 import Heading from "../../components/Heading/Heading";
 import RoundButton from "../../components/Buttons/RoundButton/RoundButton";
@@ -12,13 +13,15 @@ const WelcomePage = () => {
       <div className="welcome">
         <h1>Welcome To My </h1>
         <h1>Favourite Recipes</h1>
-        <p className="welcome__text">I gathered all of the recipes I love here</p>
+        <p className="welcome__text">
+          I gathered all of the recipes I love here
+        </p>
         <p className="welcome__text">...They can be your favourite too!</p>
-        <RoundButton />
+        <Link to="/recipes">
+          <RoundButton />
+        </Link>
       </div>
-      <div>
-        
-      </div>
+      <div></div>
     </div>
   );
 };
