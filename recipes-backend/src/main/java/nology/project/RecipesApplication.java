f3ff883e1a4b999f5f70e63443547f375b970bf1
Piyaporn <piyapornlahr@gmail.com>
2023-06-02 09:58:21 +0100

    Re-created backend

--- recipes-backend/src/main/java/nology/project/RecipesApplication.java
+++ recipes-backend/src/main/java/nology/project/RecipesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RecepiesApplication {
+public class RecipesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RecepiesApplication.class, args);
+		SpringApplication.run(RecipesApplication.class, args);
 	}
 
 }
---
+++ recipes-backend/src/main/java/nology/project/RecipesRepository.java
@@ -0,0 +1,4 @@
+package nology.project;
+
+public class RecipesRepository {
+}
