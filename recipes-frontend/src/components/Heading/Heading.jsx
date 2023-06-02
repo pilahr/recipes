c6984a2148ba93bce4e22748a027ef60e79b2c93
Piyaporn <piyapornlahr@gmail.com>
2023-06-02 14:26:05 +0100

    added link to home icon

--- recipes-frontend/src/components/Heading/Heading.jsx
+++ recipes-frontend/src/components/Heading/Heading.jsx
@@ -1,16 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Heading.scss";
 import Menu from "../../assets/images/menu.png";
-import Home from "../../assets/images/home.png"
+import Home from "../../assets/images/home.png";
 
 const Heading = () => {
   return (
     <div>
       <div className="heading">
-        <img className="heading__img-home" src={Home} alt="home icon" />
+        <Link to="/">
+          <img className="heading__img-home" src={Home} alt="home icon" />
+        </Link>
+
         <h3 className="heading__header">Piya's Favourite Recipes</h3>
-        <img className="heading__img-menu" src={Menu} alt="menu icon" />
-        
+
+        <Link>
+          <img className="heading__img-menu" src={Menu} alt="menu icon" />
+        </Link>
       </div>
     </div>
   );
