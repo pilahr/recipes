4a61feb6c35b0afbf55adfa9debe222097cf4633
Piyaporn <piyapornlahr@gmail.com>
2023-06-02 15:30:13 +0100

    Created Roundbutton component for WelcomePage

--- recipes-frontend/src/pages/WelcomePage/WelcomePage.jsx
+++ recipes-frontend/src/pages/WelcomePage/WelcomePage.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import "./WelcomePage.scss";
-import Heading from "../../components/Heading/Heading"
-
+import Heading from "../../components/Heading/Heading";
+import RoundButton from "../../components/Buttons/RoundButton/RoundButton";
 
 const WelcomePage = () => {
-  return <div>
-    <div><Heading /></div>
+  return (
     <div>
-      <h1>Welcome To My </h1>
-      <h1>Favourite Recipes</h1>
-      <p>I gathered all of the recipes I love here</p>
-      <p>...They can be your favourite too!</p>
+      <div>
+        <Heading />
+      </div>
+      <div>
+        <h1>Welcome To My </h1>
+        <h1>Favourite Recipes</h1>
+        <p>I gathered all of the recipes I love here</p>
+        <p>...They can be your favourite too!</p>
+      </div>
+      <div>
+        <RoundButton />
+      </div>
     </div>
-  </div>
+  );
 };
 
 export default WelcomePage;
