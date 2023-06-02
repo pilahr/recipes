e9bc602243b71f2d2aeb35e9886b85040257adb3
Piyaporn <piyapornlahr@gmail.com>
2023-06-02 09:45:55 +0100

    Created files that needed for the frontend application

---
+++ recipes-frontend/src/pages/Foods/Foods.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Foods.scss";
+
+const Foods = () => {
+  return <div>Foods</div>;
+};
+
+export default Foods;
---
+++ recipes-frontend/src/pages/FoodsRecipes/FoodsRecipes.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ",/FoodsRecipes.scss"
+
+const FoodsRecipes = () => {
+  return (
+    <div>FoodsRecipes</div>
+  )
+}
+
+export default FoodsRecipes
