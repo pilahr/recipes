0bf3524f0bf3d58fc9ef0d059727cdf910e832b0
Piyaporn <piyapornlahr@gmail.com>
2023-06-06 17:21:55 +0100

    Created FoodCardList component with data from API fetched and mapped over

---
+++ recipes-frontend/src/components/FoodCardList/FoodCardList.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./FoodCardList.scss";
+import FoodCard from "../FoodCard/FoodCard";
+import { Link } from "react-router-dom";
+
+const FoodCardList = ({ recipes }) => {
+  return (
+    <div className="foodcard-list">
+      {recipes.map((recipe) => (
+        <FoodCard recipe={recipe} />
+
+        //   <Link key={recipe.id} to={`/recipe/edit/${recipe.id}`}>
+        //     <FoodCard recipe={recipe} />
+        //   </Link>
+      ))}
+    </div>
+  );
+};
+
+export default FoodCardList;
--- recipes-frontend/src/pages/Foods/Foods.jsx
+++ recipes-frontend/src/pages/Foods/Foods.jsx
@@ -1,11 +1,23 @@
-import { React, useState } from "react";
+import { React, useEffect, useState } from "react";
 import "./Foods.scss";
 import Heading from "../../components/Heading/Heading";
 import Searchbox from "../../components/Searchbox/Searchbox";
-import FoodCard from "../../components/FoodCard/FoodCard";
+import FoodCardList from "../../components/FoodCardList/FoodCardList";
 
 const Foods = () => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [recipes, setRecipes] = useState([]);
+
+  const getRecipes = async () => {
+    let url = "http://localhost:8080/recipes";
+    const response = await fetch(url);
+    const recipeData = await response.json();
+    setRecipes(recipeData);
+  };
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
 
   const handleSearchInput = (event) => {
     const lowerCaseSearchedResult = event.target.value.toLowerCase();
@@ -17,11 +29,13 @@ const Foods = () => {
         <Heading />
       </div>
       <div>
-        <Searchbox searchTerms={searchTerm}
-        handleSearchInput = {handleSearchInput}/>
+        <Searchbox
+          searchTerms={searchTerm}
+          handleSearchInput={handleSearchInput}
+        />
       </div>
       <div>
-        <FoodCard />
+        <FoodCardList recipes={recipes} />
       </div>
     </div>
   );
