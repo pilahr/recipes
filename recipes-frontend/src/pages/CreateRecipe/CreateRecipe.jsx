3b26aee803a62a68e09b6798411220eb8023eb6a
Piyaporn <piyapornlahr@gmail.com>
2023-06-08 19:09:13 +0100

    Fixed CreateRecipe page and form but functionality to add is not working

--- recipes-frontend/src/pages/CreateRecipe/CreateRecipe.jsx
+++ recipes-frontend/src/pages/CreateRecipe/CreateRecipe.jsx
@@ -2,14 +2,53 @@ import React from "react";
 import "./CreateRecipe.scss";
 import CreateForm from "../../components/CreateForm/CreateForm";
 import Heading from "../../components/Heading/Heading";
+import { useNavigate } from "react-router-dom";
 
-const CreateRecipe = () => {
+const CreateRecipe = ({ vegans, levels }) => {
+  const navigate = useNavigate();
+
+  const handleSubmit = async (recipe) => {
+    const result = await fetch("http://localhost:8080/recipe", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(recipe),
+    });
+
+
+    if (result.ok) {
+      alert("Recipe added");
+      const recipe = await result.json();
+      navigate("/recipe/edit/" + recipe.id, { state: recipe });
+    } else {
+      const message = await result.text();
+      alert(message);
+    }
+  };
+
+  const defaultFormState = {
+    foodName: "",
+    ingredients: "",
+    method: "",
+    nationality: "",
+    imageUrl: "",
+    level: "",
+    vegan: "",
+  };
+
   return (
     <div className="create-recipe-page">
       <Heading className="create-recipe-page__heading" />
       <div className="create-recipe-page__form">
         <></>
-        <CreateForm title="Create the Recipe" />
+        <CreateForm
+          title="Create the Recipe"
+          handleSubmit={handleSubmit}
+          defaultFormState={defaultFormState}
+          vegans={vegans}
+          levels={levels}
+        />
       </div>
     </div>
   );
