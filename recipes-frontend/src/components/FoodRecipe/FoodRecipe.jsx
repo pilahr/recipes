ab5b966e555733edbc5c1a3ff0ebe42e34adb507
Piyaporn <piyapornlahr@gmail.com>
2023-06-19 12:20:28 +0100

    Link added on individual food recipe to go back to all recipes page

--- recipes-frontend/src/components/FoodRecipe/FoodRecipe.jsx
+++ recipes-frontend/src/components/FoodRecipe/FoodRecipe.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./FoodRecipe.scss";
+import { Link } from "react-router-dom";
 
 const FoodRecipe = ({ recipe }) => {
   const {
@@ -14,27 +15,34 @@ const FoodRecipe = ({ recipe }) => {
   // console.log(recipe);
 
   return (
-    <div className="food-recipe">
-      <div className="food-recipe__image">
-        <img
-          className="food-recipe__image--food"
-          src={imageUrl}
-          alt="food image"
-        />
-      </div>
+    <>
+      <div className="food-recipe">
+        <div className="food-recipe__image">
+          <img
+            className="food-recipe__image--food"
+            src={imageUrl}
+            alt="food image"
+          />
+        </div>
 
-      <div className="food-recipe__content">
-        <h3 className="food-recipe__content--heading">{foodName}</h3>
-        <p className="food-recipe__content--level">Difficulty: {name}</p>
-        <p className="food-recipe__content--vegan">{vegan.name}</p>
-        <h4 className="food-recipe__content--head">Nationality</h4>
-        <p className="food-recipe__content--text">{nationality}</p>
-        <h4 className="food-recipe__content--head">Ingredients</h4>
-        <p className="food-recipe__content--text">{ingredients}</p>
-        <h4 className="food-recipe__content--head">Method</h4>
-        <p className="food-recipe__content--text">{method}</p>
+        <div className="food-recipe__content">
+          <h3 className="food-recipe__content--heading">{foodName}</h3>
+          <p className="food-recipe__content--level">Difficulty: {name}</p>
+          <p className="food-recipe__content--vegan">{vegan.name}</p>
+          <h4 className="food-recipe__content--head">Nationality</h4>
+          <p className="food-recipe__content--text">{nationality}</p>
+          <h4 className="food-recipe__content--head">Ingredients</h4>
+          <p className="food-recipe__content--text">{ingredients}</p>
+          <h4 className="food-recipe__content--head">Method</h4>
+          <p className="food-recipe__content--text">{method}</p>
+        </div>
       </div>
-    </div>
+      <Link to={`/recipes`}>
+        <div className="link-back">
+          <h3>&lt; &lt; Back to all recipes</h3>
+        </div>
+      </Link>
+    </>
   );
 };
 
